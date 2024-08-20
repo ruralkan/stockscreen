06e598c834891e2c91f53bdc114edf86d328f461
ruralkan <perales26@hotmail.com>
2024-08-20 14:38:34 -0600

    Adding  json scripts functionalities to post and retrive database information

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import models
+from models import Stock
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from models import Stock
+import yfinance
 
 app=FastAPI()
-
+#Create all the database table extended the base model for all the models that we will create it
 models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory='templates')
@@ -23,17 +24,61 @@ def get_db():
     finally:
         db.close()
 
+def fetch_stock_data(id:int):
+    db=SessionLocal()
+    stock=db.query(Stock).filter(Stock.id==id).first()
+    
+    yahoo_data= yfinance.Ticker(stock.symbol)
+    print(yahoo_data.info)
+    
+    stock.ma200= yahoo_data.info['twoHundredDayAverage']
+    stock.ma50= yahoo_data.info['fiftyDayAverage']
+    stock.price = yahoo_data.info['previousClose']
+    stock.forward_pe = yahoo_data.info['forwardPE']
+    stock.forward_eps = yahoo_data.info['forwardEps']
+    stock.dividend_yield = yahoo_data.info['returnOnEquity']
+
+    
+    
+    db.add(stock)
+    db.commit()
+
+
 @app.get("/")
-def dashboard(request:Request):
+def dashboard(request:Request,forward_pe =None, dividend_yield=None, ma50 = None, ma200=None, db: Session = Depends(get_db)):
     '''
     Display stock screener dashboard / homepage
     '''
-    return templates.TemplateResponse("home.html", {
-        "request":request,
-        "somevar":2})
 
+    stocks = db.query(Stock)
+
+    if forward_pe:
+        stocks = stocks.filter(Stock.forward_pe < forward_pe)
+
+    if dividend_yield:
+        stocks = stocks.filter(Stock.dividend_yield > dividend_yield)
+    
+    if ma50:
+        stocks = stocks.filter(Stock.price > Stock.ma50)
+    
+    if ma200:
+        stocks = stocks.filter(Stock.price > Stock.ma200)
+    
+    
+
+    return templates.TemplateResponse("home.html", {
+        "request": request, 
+        "stocks": stocks, 
+        "dividend_yield": dividend_yield,
+        "forward_pe": forward_pe,
+        "ma200": ma200,
+        "ma50": ma50
+    })
+
+
+#We should use async to execute the functions into background_task
 @app.post("/stock")
-def create_stock(stock_request: StockRequest, db: Session=Depends(get_db)):
+async def create_stock(stock_request: StockRequest, background_task: BackgroundTasks, db: Session=Depends(get_db)):
     '''
     Create a stock and stores it in the database
     '''
@@ -41,7 +86,7 @@ def create_stock(stock_request: StockRequest, db: Session=Depends(get_db)):
     stock.symbol = stock_request.symbol
     db.add(stock)
     db.commit()
-    
+    background_task.add_task(fetch_stock_data,stock.id)
     
     return {
         "code":"succed",
