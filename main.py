ff75dff80310545bc38d758330e6769a8598893f
ruralkan <perales26@hotmail.com>
2024-08-19 21:11:22 -0600

    Adding  html views and request

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
 
 app=FastAPI()
 
+templates = Jinja2Templates(directory='templates')
+
 @app.get("/")
-def dashboard():
-    return {"Dashboard":"Home Page"}
+def dashboard(request:Request):
+    '''
+    Display stock screener dashboard / homepage
+    '''
+    return templates.TemplateResponse("home.html", {
+        "request":request,
+        "somevar":2})
+
+@app.post("/stock")
+def create_stock():
+    '''
+    Create a stock and stores it in the database
+    '''
+    return {
+        "code":"succed",
+        "message": "Stock created"
+    }
